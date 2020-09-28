bafa7132cf00435fe178d37307db115a38c41d48
CMDR Garbage <26555@ma-web.nl>
2020-09-28 18:56:57 +0200

    Constants.cs

---
+++ Constants.cs
@@ -0,0 +1,17 @@
+// Constants
+using UnityEngine;
+
+public class Constants : MonoBehaviour
+{
+	public const string slingShotBack = "SlingshotFront";
+
+	public const string gameManager = "GameManager";
+
+	public const string lineRendererFront = "LineRendererFront";
+
+	public const string lineRendererBack = "LineRendererBack";
+
+	public const string centrePoint = "CentrePoint";
+
+	public const string cameraMovement = "Main Camera";
+}
